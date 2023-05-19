e7466267c3ab4ee3a9feaa84e2453d4348917104
Victor Emanuel Sousa Lima <ve49550@gmail.com>
2023-05-19 18:37:15 -0300

    add backend aula 23

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -1,11 +1,17 @@
 import styles from './Select.module.css'
 
-export default function Select({ text, name, option, handleOnChange, value }) {
+export default function Select({ text, name, options, handleOnChange, value }) {
+
     return (
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}:</label>
             <select name={name} id={name} onChange={handleOnChange} value={value}>
-                <option>Selecione</option>
+                <option value="">Selecione uma opção</option>
+                {
+                    options.map((option) => (
+                        <option key={option.id} value={option.id}>{option.name}</option>
+                    ))
+                }
             </select>
         </div>
     )
--- src/components/projects/ProjectForm.js
+++ src/components/projects/ProjectForm.js
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react'
+
 import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
@@ -5,11 +7,25 @@ import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
 export default function ProjectForm({ btnText }) {
+
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:5001/categories', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(response => response.json())
+            .then(data => setCategories(data))
+            .catch(error => console.log(error))
+    }, [])
+
     return (
         <form className={styles.form}>
             <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do seu projeto" />
             <Input type="number" text="Orçamento do projeto" name="budget" placeholder="Insira a orçamento total do seu projeto" />
-            <Select text="Categoria do projeto" name="category_id" option="Selecione a categoria" />
+            <Select text="Categoria do projeto" name="category_id" options={categories} />
 
             <SubmitButton text={btnText} />
         </form>
