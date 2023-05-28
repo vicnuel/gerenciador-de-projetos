90852ab103ad5dc333293bd161230b42623dd1dd
Victor Emanuel <78376383+vicnuel@users.noreply.github.com>
2023-05-28 12:33:12 -0300

    add função adicionar serviço

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,11 +1,15 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
+import { parse, v4 as uuidv4 } from 'uuid'
+
 import Loading from '../layout/Loading'
 import Container from '../layout/Container'
 import Message from '../layout/Message'
 
 import ProjectForm from '../projects/ProjectForm'
+
+import ServiceForm from '../service/ServiceForm'
 
 import styles from './Project.module.css'
 
@@ -42,6 +46,52 @@ function Project() {
         setShowServiceForm(!showServiceForm)
     }
 
+    function createService() {
+        setMessage()
+
+        //last service
+        const lastService = project.services[project.services.length - 1]
+
+        lastService.id = uuidv4()
+
+        const lastServiceCost = lastService.cost
+
+        const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost)
+
+        // maximum validation
+        if (newCost > parseFloat(project.budget)) {
+
+            //mensagem
+            setMessage("O valor de orçamento não pode ser menor que o valor de custo")
+            setType("erro")
+            project.services.pop()
+
+            setTimeout(() => {
+                setMessage()
+                setType()
+            }, 3000)
+
+            return false
+        }
+
+        // add service cost to project total cost
+        project.cost = newCost
+
+        // update project
+        fetch(`http://localhost:5001/projects/${id}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(project)
+        }).then(rest => rest.json())
+            .then(data => {
+                // exibir serviço
+                console.log(data)
+            })
+            .catch(err => console.log(err))
+    }
+
     function ediPost(project) {
 
         //setMessage("")
@@ -50,8 +100,6 @@ function Project() {
             //mensagem
             setMessage("O valor de orçamento não pode ser menor que o valor de custo")
             setType("erro")
-
-            // apagar time
 
             setTimeout(() => {
                 setMessage()
@@ -134,7 +182,13 @@ function Project() {
                                     {
 
                                         showServiceForm &&
-                                        <p>Formulário do serviço</p>
+                                        (
+                                            <ServiceForm
+                                                handleSubmit={createService}
+                                                btnText="Adicionar serviço"
+                                                projectData={project}
+                                            />
+                                        )
                                     }
                                 </div>
                                 <h2>Serviços</h2>
